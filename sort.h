49c96576324643e02c9249d9660735c82c1528b7
MrtnOmwenga <martin36449@gmail.com>
2023-03-23 20:10:59 -0700

    Insertion sort

--- sort.h
+++ sort.h
@@ -23,9 +23,11 @@ struct listint_s *next;
 
 /* Prototypes */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 /* Utilities */
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2);
 
 #endif
