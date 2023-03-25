47244090c30e1c4f87bf8e4d945a9641bebec666
MrtnOmwenga <martin36449@gmail.com>
2023-03-25 09:35:20 -0700

    Betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,38 +9,41 @@
 
 void insertion_sort_list(listint_t **list)
 {
-  listint_t *curr, *tmp, *p;
-  if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
+listint_t *curr, *tmp, *p;
+if (list == NULL || *list == NULL || (*list)->next == NULL)
+{
+return;
+}
 
-  curr = (*list)->next;
-  while (curr != NULL) {
-    tmp = curr->prev;
-    p = curr;
+curr = (*list)->next;
+while (curr != NULL)
+{
+tmp = curr->prev;
+p = curr;
 
-    while (tmp != NULL && tmp->n > p->n) {
-      /* Swap nodes */
-      p->prev = tmp->prev;
-      if (p->prev != NULL)
-	{
-	  p->prev->next = p;
-	}
-      else
-	{
-	  *list = p;
-	}
-      tmp->next = p->next;
-      if (tmp->next != NULL)
-	{
-	  tmp->next->prev = tmp;
-	}
-      p->next = tmp;
-      tmp->prev = p;
-      tmp = p->prev;
-      /* Print list after each swap */
-      print_list(*list);
-    }
-    
-    curr = curr->next;
-  }
+while (tmp != NULL && tmp->n > p->n)
+{
+/* Swap nodes */
+p->prev = tmp->prev;
+if (p->prev != NULL)
+{
+p->prev->next = p;
+}
+else
+{
+*list = p;
+}
+tmp->next = p->next;
+if (tmp->next != NULL)
+{
+tmp->next->prev = tmp;
+}
+p->next = tmp;
+tmp->prev = p;
+tmp = p->prev;
+/* Print list after each swap */
+print_list(*list);
+}
+curr = curr->next;
+}
 }
